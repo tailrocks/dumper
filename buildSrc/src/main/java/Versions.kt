a8197ff2088f6f63faef1f007751845a4e855bb3
Alexey Zhokhov <alexey@zhokhov.com>
2021-03-20 04:12:46 +0800

    Dependencies up.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,12 +2,12 @@ object Versions {
 
     // Languages
 
-    const val kotlin = "1.4.21-2"
+    const val kotlin = "1.4.31"
 
     // Gradle plugins
 
     // https://plugins.gradle.org/plugin/com.github.ben-manes.versions
-    const val gradleVersionsPlugin = "0.36.0"
+    const val gradleVersionsPlugin = "0.38.0"
 
     // https://plugins.gradle.org/plugin/com.adarshr.test-logger
     const val gradleTestLoggerPlugin = "2.1.1"
@@ -19,35 +19,35 @@ object Versions {
     const val gradleBintrayPlugin = "1.8.5"
 
     // https://plugins.gradle.org/plugin/com.diffplug.spotless
-    const val gradleSpotlessPlugin = "5.9.0"
+    const val gradleSpotlessPlugin = "5.11.0"
 
     // https://plugins.gradle.org/plugin/nu.studer.jooq
-    const val gradleJooqPlugin = "5.2"
+    const val gradleJooqPlugin = "5.2.1"
 
     // https://plugins.gradle.org/plugin/com.gorylenko.gradle-git-properties
     const val gradleGitPropertiesPlugin = "2.2.4"
 
     // https://plugins.gradle.org/plugin/com.apollographql.apollo
-    const val gradleApolloPlugin = "2.5.2"
+    const val gradleApolloPlugin = "2.5.4"
 
     // https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow
     const val gradleShadowPlugin = "6.1.0"
 
     // https://plugins.gradle.org/plugin/io.micronaut.application
-    const val gradleMicronautPlugin = "1.3.2"
+    const val gradleMicronautPlugin = "1.4.2"
 
     // Libraries
 
-    const val micronaut = "2.3.0"
+    const val micronaut = "2.4.1"
 
     // UPDATE together with Micronaut
-    // https://repo1.maven.org/maven2/io/micronaut/micronaut-bom/2.3.0/micronaut-bom-2.3.0.pom
+    // https://repo1.maven.org/maven2/io/micronaut/micronaut-bom/2.4.1/micronaut-bom-2.4.1.pom
     // https://repo1.maven.org/maven2/io/micronaut/sql/micronaut-jooq/3.4.0/micronaut-jooq-3.4.0.pom
     // https://repo1.maven.org/maven2/io/grpc/grpc-bom/1.33.1/grpc-bom-1.33.1.pom
     // https://repo1.maven.org/maven2/io/grpc/grpc-protobuf/1.33.1/grpc-protobuf-1.33.1.pom
     const val postgresql = "42.2.18"
     const val grpc = "1.33.1"
-    const val flyway = "7.4.0"
+    const val flyway = "7.5.3"
     const val jooq = "3.14.4"
     const val protobuf = "3.14.0"
     const val spotbugs = "4.0.3"
@@ -58,9 +58,9 @@ object Versions {
     const val spotbugsAnnotations = "4.2.0"
     const val junit = "5.6.2"
     const val slugify = "2.4"
-    const val graphQlDateTime = "3.0.0"
+    const val graphQlDateTime = "4.0.0"
     const val graphQlTools = "11.0.0"
-    const val apollo = "2.5.2"
+    const val apollo = "2.5.4"
 
     const val jambalayaChecks = "0.1.4"
     const val jambalayaChecksJooq = "0.1.0"
