1370c9ff0ab87cfabf7f18fd8d3ee4ae7f103ae9
Alexey Zhokhov <alexey@zhokhov.com>
2021-06-15 12:48:40 +0800

    Cleanup.

--- dumper-api/build.gradle.kts
+++ dumper-api/build.gradle.kts
@@ -60,9 +60,6 @@ dependencies {
     implementation("com.graphql-java-kickstart:graphql-java-tools:${Versions.graphQlTools}")
     implementation("com.zhokhov.graphql:graphql-java-datetime:${Versions.graphQlDateTime}")
 
-    // TODO temp workaround for overriding dependency version
-    implementation("com.graphql-java:graphql-java:15.0")
-
     // Jambalaya
     implementation("io.github.expatiat.jambalaya:jambalaya-graphql:${Versions.jambalayaGraphql}")
     implementation("io.github.expatiat.jambalaya:jambalaya-graphql-jooq:${Versions.jambalayaGraphqlJooq}")
